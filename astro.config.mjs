7f0423d28dfe0ce679d992b96e097975f8eb25a1
Giovanni Birkelund <gio@Giovannis-MacBook-Air.local>
2023-10-09 23:57:19 -0600

    change vercel to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,34 @@ import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import checker from 'vite-plugin-checker';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
-	output: 'hybrid',
-	adapter: vercel({
-		functionPerRoute: false,
-	}),
-	// eslint-disable-next-line no-undef
-	site: process.env.SITE_URL,
-	vite: {
-		plugins: [
-			checker({
-				typescript: true,
-				overlay: { initialIsOpen: false, badgeStyle: 'left: 55px; bottom: 8px;' },
-				enableBuild: false, // we already check that in `yarn ci:check`
-			}),
-		],
-		optimizeDeps: {
-			exclude: ['@resvg/resvg-js'],
-		},
-		build: {
-			sourcemap: true /* B2B:CONFIG consider disabling sourcemaps for production */,
-		},
-	},
-	server: {
-		port: 3000,
-	},
+  integrations: [react()],
+  output: 'hybrid',
+  adapter: netlify(),
+  // eslint-disable-next-line no-undef
+  site: process.env.SITE_URL,
+  vite: {
+    plugins: [checker({
+      typescript: true,
+      overlay: {
+        initialIsOpen: false,
+        badgeStyle: 'left: 55px; bottom: 8px;'
+      },
+      enableBuild: false // we already check that in `yarn ci:check`
+    })],
+
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js']
+    },
+    build: {
+      sourcemap: true /* B2B:CONFIG consider disabling sourcemaps for production */
+    }
+  },
+
+  server: {
+    port: 3000
+  }
 });
